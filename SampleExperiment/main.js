471c240103c3298725da3025cc0f96498f92502b
Yara Studer <yarastuder@gmail.com>
2025-06-30 12:36:16 -0400

    SampleExperiment - Added early termination screen and debug mode

--- SampleExperiment/main.js
+++ SampleExperiment/main.js
@@ -1,15 +1,26 @@
+//Debug mode - enable this to skip consent and demogrphic forms
+var debug = false;
+
 //Initialize
 var sub_id = Math.random().toString().substr(2, 6); // generate random 6 digit number
+var completed = false
 var jsPsych = initJsPsych({
     show_progress_bar: true,
     on_finish: function () {
-        jsPsych.endExperiment(`<p>You've finished the last task. Thanks for participating!</p>
-    <p><a href="https://app.prolific.co/submissions/complete?cc=XXXXXX">Click here to return to Prolific and complete the study</a>.</p>`);
-        console.log("End of experiment");
-        jsPsych.data.get().localSave("csv", "sub-" + sub_id + "_data.csv");
+        if(completed) {
+            // Message for successful completion of study
+            jsPsych.endExperiment(`<p>Thanks for participating!</p>
+                <p><a href="https://app.prolific.co/submissions/complete?cc=XXXXXX">Click here to return to Prolific and complete the study</a>.</p>`);
+            console.log("End of experiment");
+            jsPsych.data.get().localSave("csv", "sub-" + sub_id + "_data.csv");
+        } else {
+            // Message for early termination of study
+            jsPsych.endExperiment(`<p>Unfortunately, you are not eligible for the study.</p>`);
+            console.log("Experiment terminated early")
+        }
     },
 });
-// for info on how to save on the server, please contact me: gjf2118@columbia.edu
+// for info on how to save on the server, please contact gjf2118@columbia.edu
 var timeline = [];
 
 //Import config file
@@ -17,9 +28,12 @@ import config from "./config.js"
 
 //Adds consent form to timeline
 import { pushConsentForm } from '../consent.js';
-pushConsentForm(jsPsych, timeline, config.experimentName);
+if (!debug) {
+    pushConsentForm(jsPsych, timeline, config.experimentName)
+}
 
 //EXPERIMENT CONTENT GOES HERE
+
 var instructions = {
     type: jsPsychHtmlButtonResponse,
     stimulus: config.instructions,
@@ -30,12 +44,27 @@ var task = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: "This is an example task. Press any key."
 };
+
 timeline.push(instructions, task);
+
 //END OF EXPERIMENT CONTENT
 
 //Adds demographics survey to timeline
 import { pushDemographicSurvey } from '../demographics.js'
-pushDemographicSurvey(timeline);
+if (!debug) {
+    pushDemographicSurvey(timeline);
+}
+
+//Adds final trial to mark successful completion
+var completion = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: `You've finished the last task. Click the button to end the study.`,
+    choices: ["End"],
+    on_finish: function () {
+        completed = true
+    }
+}
+timeline.push(completion)
 
 //Run
 jsPsych.run(timeline);
